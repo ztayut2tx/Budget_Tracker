304f4a8086b291be5373298881c8113f946116fc
Zach Taylor <ztayut2tx@gmail.com>
2021-03-09 13:00:08 -0600

    created function to send staged pending data to the online database

--- public/db.js
+++ public/db.js
@@ -22,4 +22,28 @@ function saveRecord(record) {
     const transaction = db.transaction(["pending"], "readwrite");
     const store = transaction.objectStore("pending");
     store.add(record);
-}
+};
+
+function checkDatabase() {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+    const getAll = store.getAll();
+
+    getAll.onsuccess = function () {
+        if(getAll.result.length > 0) {
+            fetch("/api/transaction/bulk", {
+               method: "POST",
+               body: JSON.stringify(getAll.result),
+               headers: {
+                   Accept: "application/json, text/plain, */*",
+                   "Content-Type": "application/json"
+               } 
+            })
+                .then(response => response.json()).then(() => {
+                    const transaction = db.transaction(["pending"], "readwrite");
+                    const store = transaction.objectStore("pending");
+                    store.clear();
+                });
+        }
+    };
+} 
