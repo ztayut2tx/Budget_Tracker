5454bc4b3cfa6c1cd994a1006cdeffddf59eacdc
Zach Taylor <ztayut2tx@gmail.com>
2021-03-09 12:00:49 -0600

    set up requests in db.js

--- public/db.js
+++ public/db.js
@@ -1,3 +1,19 @@
 let db;
 
 const request = indexedDB.open("NewBudget", 1);
+
+request.onupgradeneeded = function (event) {
+    const db = event.target.result;
+    db.createObjectStore("pending", { autoIncrement: true});
+};
+
+request.onsuccess = function (event) {
+    db = event.target.result;
+    if(navigator.onLine) {
+        checkDB();
+    }
+};
+
+request.onError = function (event) {
+    console.log("Error!" + event.target.errorCode);
+};
